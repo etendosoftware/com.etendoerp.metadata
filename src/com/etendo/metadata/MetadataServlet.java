92a799469b1e18f5b6fd3eafe4517c7d04263120
Luciano Rocha <luciano.rocha@etendo.software>
2024-08-21 15:25:17 -0300

    Feature ETP-237: Add initial metadata servlet

---
+++ src/com/etendo/metadata/MetadataServlet.java
@@ -0,0 +1,17 @@
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "MetadataServlet", urlPatterns = {"/com.etendo.metadata"})
+public class MetadataServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        resp.getWriter().println("<h1>Hello, this is your servlet!</h1>");
+    }
+}
