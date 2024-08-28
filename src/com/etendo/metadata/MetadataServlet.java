f1e672938b5b0a8e7bdbe06a147980e0aceeea58
Luciano Rocha <luciano.rocha@etendo.software>
2024-08-28 01:07:30 -0300

    Feature ETP-237: Minor fixes

--- src/com/etendo/metadata/MetadataServlet.java
+++ src/com/etendo/metadata/MetadataServlet.java
@@ -1,17 +1,60 @@
+package com.etendo.metadata;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Optional;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
-@WebServlet(name = "MetadataServlet", urlPatterns = {"/com.etendo.metadata"})
-public class MetadataServlet extends HttpServlet {
+import com.smf.securewebservices.SWSConfig;
+import org.apache.commons.io.IOUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.codehaus.jettison.json.JSONArray;
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+import org.hibernate.criterion.Restrictions;
+import org.jfree.chart.HashUtilities;
+import org.openbravo.authentication.hashing.PasswordHash;
+import org.openbravo.base.HttpBaseServlet;
+import org.openbravo.dal.core.OBContext;
+import org.openbravo.dal.service.OBCriteria;
+import org.openbravo.dal.service.OBDal;
+import org.openbravo.erpCommon.utility.Utility;
+import org.openbravo.model.ad.access.Role;
+import org.openbravo.model.ad.access.User;
+import org.openbravo.model.common.enterprise.Organization;
+import org.openbravo.model.common.enterprise.Warehouse;
+import org.openbravo.service.db.DalConnectionProvider;
+import org.openbravo.utils.FormatUtilities;
 
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("text/html");
-        resp.getWriter().println("<h1>Hello, this is your servlet!</h1>");
-    }
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.smf.securewebservices.utils.SecureWebServicesUtils;
+import org.openbravo.base.secureApp.AllowedCrossDomainsHandler;
+
+/**
+ * 
+ * @author androettop
+ */
+public class MetadataServlet extends HttpBaseServlet {
+	private static final String APPLICATION_JSON = "application/json";
+	private static final long serialVersionUID = 1L;
+	static final long ONE_MINUTE_IN_MILLIS = 60000;
+	private static final Logger log = LogManager.getLogger();
+
+	public void doOptions(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+		AllowedCrossDomainsHandler.getInstance().setCORSHeaders(request, response);
+	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+		JSONObject result = new JSONObject();
+		AllowedCrossDomainsHandler.getInstance().setCORSHeaders(request, response);
+		OBContext.setAdminMode(true);
+		response.setContentType(APPLICATION_JSON);
+		Writer out = response.getWriter();
+		out.write(result.toString());
+		out.close();
+	}
 }
