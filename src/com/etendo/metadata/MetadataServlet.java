872fb4fe1c897011528b5d2137cead6d8e933ce1
Luciano Rocha <luciano.rocha@etendo.software>
2024-08-28 17:12:51 -0300

    Feature ETP-90: Working on metadata module

--- src/com/etendo/metadata/MetadataServlet.java
+++ src/com/etendo/metadata/MetadataServlet.java
@@ -1,60 +1,202 @@
 package com.etendo.metadata;
 
-import java.io.IOException;
-import java.io.Writer;
-import java.util.Optional;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.smf.securewebservices.utils.SecureWebServicesUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+import org.hibernate.criterion.Restrictions;
+import org.openbravo.authentication.AuthenticationManager;
+import org.openbravo.base.HttpBaseServlet;
+import org.openbravo.base.exception.OBException;
+import org.openbravo.base.secureApp.AllowedCrossDomainsHandler;
+import org.openbravo.base.session.OBPropertiesProvider;
+import org.openbravo.dal.core.OBContext;
+import org.openbravo.dal.service.OBCriteria;
+import org.openbravo.dal.service.OBDal;
+import org.openbravo.database.SessionInfo;
+import org.openbravo.model.ad.access.Role;
+import org.openbravo.model.ad.access.WindowAccess;
+import org.openbravo.model.ad.ui.Menu;
+import org.openbravo.model.ad.ui.Window;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import com.smf.securewebservices.SWSConfig;
-import org.apache.commons.io.IOUtils;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.codehaus.jettison.json.JSONArray;
-import org.codehaus.jettison.json.JSONException;
-import org.codehaus.jettison.json.JSONObject;
-import org.hibernate.criterion.Restrictions;
-import org.jfree.chart.HashUtilities;
-import org.openbravo.authentication.hashing.PasswordHash;
-import org.openbravo.base.HttpBaseServlet;
-import org.openbravo.dal.core.OBContext;
-import org.openbravo.dal.service.OBCriteria;
-import org.openbravo.dal.service.OBDal;
-import org.openbravo.erpCommon.utility.Utility;
-import org.openbravo.model.ad.access.Role;
-import org.openbravo.model.ad.access.User;
-import org.openbravo.model.common.enterprise.Organization;
-import org.openbravo.model.common.enterprise.Warehouse;
-import org.openbravo.service.db.DalConnectionProvider;
-import org.openbravo.utils.FormatUtilities;
-
-import com.auth0.jwt.interfaces.DecodedJWT;
-import com.smf.securewebservices.utils.SecureWebServicesUtils;
-import org.openbravo.base.secureApp.AllowedCrossDomainsHandler;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.List;
 
 /**
- * 
- * @author androettop
+ *
+ * @author luuchorocha
  */
 public class MetadataServlet extends HttpBaseServlet {
-	private static final String APPLICATION_JSON = "application/json";
-	private static final long serialVersionUID = 1L;
-	static final long ONE_MINUTE_IN_MILLIS = 60000;
-	private static final Logger log = LogManager.getLogger();
+    private static final String APPLICATION_JSON = "application/json";
+    private static final Logger log = LogManager.getLogger();
+    private static Integer wsInactiveInterval = null;
+    private static final int DEFAULT_WS_INACTIVE_INTERVAL = 60;
 
-	public void doOptions(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-		AllowedCrossDomainsHandler.getInstance().setCORSHeaders(request, response);
-	}
+    @Override
+    public final void service(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        final boolean sessionExists = request.getSession(false) != null;
 
-	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-		JSONObject result = new JSONObject();
-		AllowedCrossDomainsHandler.getInstance().setCORSHeaders(request, response);
-		OBContext.setAdminMode(true);
-		response.setContentType(APPLICATION_JSON);
-		Writer out = response.getWriter();
-		out.write(result.toString());
-		out.close();
-	}
+        AllowedCrossDomainsHandler.getInstance().setCORSHeaders(request, response);
+
+        if (request.getMethod().equals("OPTIONS")) {
+            return;
+        }
+
+        if (request.getMethod().equals("GET")) {
+            response.setStatus(405);
+            return;
+        }
+
+        String authStr = request.getHeader("Authorization");
+        String token = null;
+
+        if (authStr != null && authStr.startsWith("Bearer ")) {
+            token = authStr.substring(7);
+        }
+
+        try {
+            DecodedJWT decodedToken = SecureWebServicesUtils.decodeToken(token);
+            if (decodedToken != null) {
+                String userId = decodedToken.getClaim("user").asString();
+                String roleId = decodedToken.getClaim("role").asString();
+                String orgId = decodedToken.getClaim("organization").asString();
+                String warehouseId = decodedToken.getClaim("warehouse").asString();
+                String clientId = decodedToken.getClaim("client").asString();
+                if (userId == null || userId.isEmpty() || roleId == null || roleId.isEmpty() || orgId == null
+                        || orgId.isEmpty() || warehouseId == null || warehouseId.isEmpty() || clientId == null || clientId.isEmpty()) {
+                    throw new OBException("SWS - Token is not valid");
+                }
+                log.debug("SWS accessed by userId " + userId);
+                OBContext.setOBContext(
+                        SecureWebServicesUtils.createContext(userId, roleId, orgId, warehouseId, clientId));
+                OBContext.setOBContextInSession(request, OBContext.getOBContext());
+                SessionInfo.setUserId(userId);
+                SessionInfo.setProcessType("WS");
+                SessionInfo.setProcessId("DAL");
+                try {
+                    this.doPost(request, response);
+                } finally {
+                    final boolean sessionCreated = !sessionExists && null != request.getSession(false);
+                    if (sessionCreated && AuthenticationManager.isStatelessRequest(request)) {
+                        log.warn("Stateless request, still a session was created " + request.getRequestURL() + " "
+                                + request.getQueryString());
+                    }
+
+                    HttpSession session = request.getSession(false);
+                    if (session != null) {
+                        // HttpSession for WS should typically expire fast
+                        int maxExpireInterval = getWSInactiveInterval();
+                        if (maxExpireInterval == 0) {
+                            session.invalidate();
+                        } else {
+                            session.setMaxInactiveInterval(maxExpireInterval);
+                        }
+                    }
+                }
+            } else {
+                throw new OBException("SWS - Token is not valid");
+            }
+        } catch (Exception e) {
+            JSONObject result = new JSONObject();
+            log.warn(e.getMessage());
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            Writer writer = response.getWriter();
+            try {
+                result.put("ok", false);
+            } catch (JSONException ex) {
+                throw new RuntimeException(ex);
+            }
+            writer.write(result.toString());
+        }
+    }
+
+    private int getWSInactiveInterval() {
+        if (wsInactiveInterval == null) {
+            try {
+                wsInactiveInterval = Integer.parseInt(OBPropertiesProvider.getInstance().getOpenbravoProperties()
+                        .getProperty("ws.maxInactiveInterval", Integer.toString(DEFAULT_WS_INACTIVE_INTERVAL)));
+            } catch (Exception e) {
+                wsInactiveInterval = DEFAULT_WS_INACTIVE_INTERVAL;
+            }
+            log.info("Sessions for WS calls expire after " + wsInactiveInterval
+                    + " seconds. This can be configured with ws.maxInactiveInterval property.");
+        }
+
+        return wsInactiveInterval;
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        String path = request.getPathInfo() != null ? request.getPathInfo() : "";
+
+        if (path.startsWith("/window")) {
+            this.fetchWindow(request, response);
+        } else if (path.startsWith("/translations")) {
+            this.fetchTranslations(request, response);
+        } else {
+            this.notFound(request, response);
+        }
+    }
+
+    private void fetchTranslations(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        JSONObject result = new JSONObject();
+        Writer writer = response.getWriter();
+        response.setContentType(APPLICATION_JSON);
+
+        try {
+            result.put("ok", true);
+        } catch (JSONException e) {
+            response.setStatus(500);
+        }
+
+        writer.write(result.toString());
+        writer.close();
+    }
+
+    private void fetchWindow(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        JSONObject result = new JSONObject();
+        Writer writer = response.getWriter();
+        response.setContentType(APPLICATION_JSON);
+
+        OBContext obContext = OBContext.getOBContext();
+        String currentUser = obContext.getUser().getName();
+        String currentOrganization = obContext.getCurrentOrganization().getName();
+        Role currentRole = obContext.getRole();
+
+        OBContext.setAdminMode();
+        OBCriteria<Menu> criteria = OBDal.getInstance().createCriteria(Menu.class);
+
+        try {
+            List<Menu> results = criteria.list();
+
+            result.put("menu", results);
+            result.put("ok", true);
+        } catch (JSONException e) {
+            response.setStatus(500);
+        }
+
+        OBContext.restorePreviousMode();
+
+        writer.write(result.toString());
+        writer.close();
+    }
+
+    public static boolean hasWindowAccess(String windowId) {
+        OBCriteria<WindowAccess> windowAccessCriteria = OBDal.getInstance().createCriteria(WindowAccess.class);
+        windowAccessCriteria.add(Restrictions.eq(WindowAccess.PROPERTY_ROLE, OBContext.getOBContext().getRole()));
+        windowAccessCriteria.add(Restrictions.eq(WindowAccess.PROPERTY_WINDOW, OBDal.getInstance().get(Window.class, windowId)));
+
+        return windowAccessCriteria.list().size() > 0;
+    }
+
+    private void notFound(HttpServletRequest request, HttpServletResponse response) {
+        response.setStatus(404);
+    }
 }
