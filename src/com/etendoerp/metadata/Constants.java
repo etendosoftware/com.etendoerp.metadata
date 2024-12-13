5fb91753d41075a14e4d141d1b213b5e87d317d7
Luciano Rocha <luciano.rocha@etendo.software>
2024-12-13 20:53:04 -0300

    Feature ETP-868: Fix session issues.

---
+++ src/com/etendoerp/metadata/Constants.java
@@ -0,0 +1,12 @@
+package com.etendoerp.metadata;
+
+public class Constants {
+
+    public static final String INVALID_OR_MISSING_TOKEN = "Invalid or missing token";
+    public static final String ONLY_POST_METHOD_IS_ALLOWED = "Only POST method is allowed";
+    public static final String MISSING_REQUIRED_CLAIMS = "Missing required claims: userId={}, roleId={}, orgId={}, warehouseId={}, clientId={}";
+    public static final String ERROR_PROCESSING_REQUEST = "Error processing request";
+    public static final String FAILED_TO_DECODE_TOKEN = "Failed to decode token";
+    public static final String HTTP_METHOD_POST = "POST";
+    public static final String HTTP_METHOD_OPTIONS = "OPTIONS";
+}
