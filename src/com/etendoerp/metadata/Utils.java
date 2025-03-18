7f7f15b9380d368a9b8d76be180468ac9840f702
Luciano Rocha <luciano.rocha@etendo.software>
2025-03-18 19:21:18 -0300

    Feature ETP-1306: Added server side display logic evaluation to TabBuilder.

--- src/com/etendoerp/metadata/Utils.java
+++ src/com/etendoerp/metadata/Utils.java
@@ -1,9 +1,13 @@
 package com.etendoerp.metadata;
 
 import org.hibernate.criterion.Restrictions;
+import org.openbravo.base.expression.OBScriptEngine;
+import org.openbravo.client.application.DynamicExpressionParser;
 import org.openbravo.dal.service.OBDal;
 import org.openbravo.model.ad.system.Language;
+import org.openbravo.model.ad.ui.Field;
 
+import javax.script.ScriptException;
 import javax.servlet.http.HttpServletRequest;
 import java.util.Arrays;
 
@@ -24,6 +28,29 @@ public class Utils {
                                .add(Restrictions.eq(Language.PROPERTY_ACTIVE, true))
                                .add(Restrictions.eq(Language.PROPERTY_LANGUAGE, languageCode))
                                .uniqueResult();
+    }
+
+    public static boolean evaluateDisplayLogicAtServerLevel(Field field) {
+        String displayLogicEvaluatedInTheServer = field.getDisplayLogicEvaluatedInTheServer();
+
+        if (displayLogicEvaluatedInTheServer == null) {
+            return true;
+        }
+
+        String translatedDisplayLogic =
+                DynamicExpressionParser.replaceSystemPreferencesInDisplayLogic(displayLogicEvaluatedInTheServer);
+
+        boolean result;
+
+        DynamicExpressionParser parser = new DynamicExpressionParser(translatedDisplayLogic, field.getTab());
+
+        try {
+            result = (Boolean) OBScriptEngine.getInstance().eval(parser.getJSExpression());
+        } catch (ScriptException e) {
+            result = true;
+        }
+
+        return result;
     }
 
 }
