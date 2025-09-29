78694ed5c9a2f48d91803d43c83d6235d41c9942
Santiago Alaniz <santiago.alaniz@etendo.software>
2025-09-29 16:06:02 -0300

    Feature ETP-2476: Fix import

--- src/com/etendoerp/metadata/builders/TabBuilder.java
+++ src/com/etendoerp/metadata/builders/TabBuilder.java
@@ -20,7 +20,7 @@ package com.etendoerp.metadata.builders;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.codehaus.jettison.json.JSONArray;
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
