e70fdcc2235cccf6cc91c7846caf0ce8aeb4e6c4
Santiago Alaniz <santiago.alaniz@etendo.software>
2025-10-20 09:42:49 -0300

    Feature ETP-2726: update lang to lang3

--- src/com/etendoerp/metadata/http/NotesServlet.java
+++ src/com/etendoerp/metadata/http/NotesServlet.java
@@ -27,7 +27,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.http.HttpStatus;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
